e19dead54bf3b2099d70d91d1f77f712c7a1e3db
Mohamed Mayla <mohamed.mayla@gmail.com>
2021-03-17 09:37:14 +0100

    feat: create normalizers

--- src/utils/normalizers.ts
+++ src/utils/normalizers.ts
@@ -1,2 +1,16 @@
+import { Post } from './reddit'
+
 export const arrayToLowercase = (list: Array<string | number>): string[] =>
   list.map((item) => item.toString().toLowerCase())
+
+export const normalizeNumberToK = (n: number): string =>
+  n >= 1000 ? Math.floor(n / 1000).toString() + 'k' : n.toString()
+
+export type NormalizedPost = Omit<Post, 'votes'> & { votes: string }
+
+export const normalizePost = (post: Post): NormalizedPost => ({
+  ...post,
+  votes: normalizeNumberToK(post.votes),
+})
+
+export const normalizePosts = (posts: Post[]): NormalizedPost[] => posts.map((p) => normalizePost(p))
